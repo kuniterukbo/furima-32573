2925116f1cc0edbca2c2cd311864e0e3448349a8
kuniterukbo <r2ka2h5e@icloud.com>
2020-12-23 02:02:47 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,       presence: true
+  validates :family_name,   presence: true
+  validates :family_name, presence: true
+  validates :first_name,   presence: true
+  validates :family_name_katakana,   presence: true
+  validates :first_name_katakana,   presence: true
+  validates :birthday,   presence: true
+
+  has_many :purchase_histories
+  has_many :items
 end
