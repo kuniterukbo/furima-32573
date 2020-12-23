adfc3e11629782a8c4fd80ffbf918ad507f4c97f
kuniterukbo <r2ka2h5e@icloud.com>
2020-12-24 00:44:04 +0900

    userモデルの単体テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,24 @@ class User < ApplicationRecord
 
   validates :nickname,       presence: true
   validates :family_name,   presence: true
-  validates :family_name, presence: true
   validates :first_name,   presence: true
   validates :family_name_katakana,   presence: true
   validates :first_name_katakana,   presence: true
   validates :birthday,   presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+    validates :family_name
+    validates :first_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角カタカナを使用してください' } do
+    validates :family_name_katakana
+    validates :first_name_katakana
+  end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+
+
 
   has_many :purchase_histories
   has_many :items
