06c8a558b1709ac6d6fdbaed538bfd9cacfd7023
kuniterukbo <r2ka2h5e@icloud.com>
2020-12-25 17:07:31 +0900

    出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-
+  before_action :authenticate_user!, except: [:index]
   def index
   end
   
@@ -8,6 +8,19 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    binding.pry
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def item_params
+    params.require(:item).permit(:product_name, :product_explanation, :category_id, :product_state_id, :shipping_charge_id, :prefecture_id, :delivery_date_id, :selling_price, :image).merge(user_id: current_user.id)
   end
 
 end
