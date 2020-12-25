16f625eceaff727afb45631f95a2a375721abe3a
kuniterukbo <r2ka2h5e@icloud.com>
2020-12-25 14:44:28 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,16 +9,22 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :product_name
     validates :product_explanation
+
+    validates :user
+  end
+
+  with_options presence: true, numericality: { other_than: 1, message: "can't be blank"} do
     validates :category_id
     validates :product_state_id
     validates :shipping_charge_id
     validates :prefecture_id
     validates :delivery_date_id
-    validates :selling_price
-    validates :user
   end
 
-  validates :category_id, numericality: { other_than: 1 }
+  with_options presence: true, format: { with: /\A[0-9]+\z/, message: '半角数字を使用してください' } do
+    validates :selling_price
+  end
+  validates :selling_price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
 
   has_one :purchase_history
   belongs_to :user
