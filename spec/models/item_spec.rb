ee0a3a22e589e472b67892bf9e0a66dac32d94b2
kuniterukbo <r2ka2h5e@icloud.com>
2020-12-27 17:55:48 +0900

    モデル単体テストコード修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -56,13 +56,38 @@ RSpec.describe Item, type: :model do
           @item.valid?
           expect(@item.errors.full_messages).to include("Selling price can't be blank")
         end
+        it "カテゴリーが1（--）の場合は出品できない" do
+          @item.category_id = 1
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Category can't be blank")
+        end
+        it "商品の状態の情報が1（--）の場合は出品できない" do
+          @item.product_state_id = 1
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Product state can't be blank")
+        end
+        it "配送料負担の情報が1（--）の場合は出品できない" do
+          @item.shipping_charge_id = 1
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Shipping charge can't be blank")
+        end
+        it "配送元の地域の情報が1（--）の場合は出品できない" do
+          @item.prefecture_id = 1
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+        end
+        it "配送までの日数の情報が1（--）の場合は出品できない" do
+          @item.delivery_date_id = 1
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Delivery date can't be blank")
+        end
         it "価格が¥300未満では出品できない" do
-          @item.selling_price = "299"
+          @item.selling_price = 299
           @item.valid?
           expect(@item.errors.full_messages).to include("Selling price is out of setting range")
         end
         it "価格が¥10,000,000以上たど出品できない" do
-          @item.selling_price = "10000000"
+          @item.selling_price = 10000000
           @item.valid?
           expect(@item.errors.full_messages).to include("Selling price is out of setting range")
         end
@@ -71,7 +96,7 @@ RSpec.describe Item, type: :model do
           @item.valid?
           expect(@item.errors.full_messages).to include("Selling price is out of setting range")
         end
-        it "価格が文字では登録できない" do
+        it "価格が文字では出品できない" do
           @item.selling_price = "aaa"
           @item.valid?
           expect(@item.errors.full_messages).to include("Selling price is out of setting range")
