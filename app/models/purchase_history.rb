835a18d9d1df64d9313456987986b36b9552b707
kuniterukbo <r2ka2h5e@icloud.com>
2021-01-06 16:23:48 +0900

    バリデーションの設定

--- app/models/purchase_history.rb
+++ app/models/purchase_history.rb
@@ -1,2 +1,13 @@
 class PurchaseHistory < ApplicationRecord
+
+
+  with_options presence: true do
+    validates :user
+    validates :item
+  end
+
+  belongs_to :user
+  belongs_to :item
+  has_one :shipping_address
+
 end
--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,2 +1,13 @@
 class ShippingAddress < ApplicationRecord
+
+  with_options presence: true do
+    validates :postal_code
+    validates :prefecture_id 
+    validates :municipality
+    validates :address 
+    validates :phone_number
+    validates :purchase_history
+  end
+
+  belongs_to :purchase_history
 end
